4d384afe8b65eed20b524605b1b1199bd8140daf
cpaulisi <cullen.paulisick@optum.com>
2021-10-12 10:52:30 -0400

    updated setup.py again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'optumpublicdata',         # How you named your package folder (MyLib)
   packages = ['optumpublicdata'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'package for automating the extract of public data related to COVID-19',   # Give a short description about your library
   author = 'Cullen Paulisick',                   # Type in your name
